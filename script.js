d95cc038942fa848256fea80ea25327bec2b301d
siddhant-shekhar <104164130+siddhant-shekhar@users.noreply.github.com>
2023-08-21 05:50:35 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+function show() {
+  document.getElementById("one").style.display = "inline";
+  document.getElementById("two").style.display = "inline";
+  document.getElementById("three").style.display = "inline";
+  document.getElementById("four").style.display = "inline";
+  document.getElementById("five").style.display = "inline";
+}
+function hide() {
+  document.getElementById("one").style.display = "none";
+  document.getElementById("two").style.display = "none";
+  document.getElementById("three").style.display = "none";
+  document.getElementById("four").style.display = "none";
+  document.getElementById("five").style.display = "none";
+}
